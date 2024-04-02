68d3b52e31f37160ce8a6eba85e4b9c8290bece9
italomoraess <italomoraes.sth@gmail.com>
2024-04-02 11:10:12 -0300

    create layout product page

--- src/app/(store)/(home)/page.tsx
+++ src/app/(store)/(home)/page.tsx
@@ -20,7 +20,7 @@ export default async function Home() {
     <div className="grid max-h-[860px] grid-cols-9 grid-rows-6 gap-6">
       <Link
         className="group relative col-span-6 row-span-6 rounded-lg bg-zinc-900 overflow-hidden flex justify-center items-end"
-        href={`/products/${highLightedProduct.slug}`}
+        href={`/product/${highLightedProduct.slug}`}
       >
         <Image
           src={highLightedProduct.image}
@@ -48,7 +48,7 @@ export default async function Home() {
         return (
           <Link
             className="group relative col-span-3 row-span-3 rounded-lg bg-zinc-900 overflow-hidden flex justify-center items-end"
-            href={`/products/${product.slug}`}
+            href={`/product/${product.slug}`}
             key={product.id}
           >
             <Image
